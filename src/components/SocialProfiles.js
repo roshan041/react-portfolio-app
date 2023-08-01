2121faf42ccea8ed1d7aa1f58fdc624482e17dc5
Roshan Bhagat <roshanbhagat041@gmail.com>
2023-08-01 20:14:48 +0530

    social profile images added

--- src/components/SocialProfiles.js
+++ src/components/SocialProfiles.js
@@ -39,28 +39,28 @@ class SocialProfiles extends Component{
 
 
 //stateless components
-const SocialProfile=props=>{
-    
-    const {link,image}= props.socialProfile;
-    return(
+const SocialProfile = props => {
+
+    const { link, image } = props.socialProfile;
+    return (
         <span>
             <a href={link}>
-                <img scr={image} alt='social profile' style={{width:35, height:35, margin:10}} />
+                <img src={image} alt='social profile' style={{ width: 35, height: 35, margin: 10 }} />
             </a>
         </span>
     )
 }
 
-const SocialProfiles=()=>{
-    return(
+const SocialProfiles = () => {
+    return (
         <div>
             <h2>Connect with me! on social profiles</h2>
             <div>
                 {
-                    SOCIAL_PROFILES.map(SOCIAL_PROFILE =>{
-                        return(
-                                <SocialProfile key={SOCIAL_PROFILE.id} socialProfile={SOCIAL_PROFILE}/>
-                            );
+                    SOCIAL_PROFILES.map(SOCIAL_PROFILE => {
+                        return (
+                            <SocialProfile key={SOCIAL_PROFILE.id} socialProfile={SOCIAL_PROFILE} />
+                        );
                     })
                 }
             </div>
